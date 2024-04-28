4de7d8b32033dc84fa1dcc8f2c6664498ae66483
phamquangviet891 <82923647+VietPam@users.noreply.github.com>
2024-04-28 11:26:30 +0700

    clean: refactor solution

--- QRCodeAttendance/Program.cs
+++ QRCodeAttendance/Program.cs
@@ -1,36 +1,39 @@
+using QRCodeAttendance.Presentation.Midllewares;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
-namespace QRCodeAttendance
+namespace QRCodeAttendance;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+        Log.Logger = new LoggerConfiguration()
+                 .MinimumLevel.Debug()
+                 .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                 .WriteTo.File("logs/mylog.txt", rollingInterval: RollingInterval.Day)
+                 .CreateLogger();
 
 
-            app.MapControllers();
+        var builder = WebApplication.CreateBuilder(args);
 
-            app.Run();
-        }
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddServices();
+        builder.Services.AddInfrastructure(builder.Configuration);
+
+        var app = builder.Build();
+
+        app.UseCors("http");
+
+        app.UseTokenMiddleware();
+        app.UseExceptionHandlerMiddleware();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
